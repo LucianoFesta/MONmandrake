91780c3b485b2fc7f59a8af1d9d49a1b6bdfe6f4
Luciano Festa <lucianoefesta@hotmail.com>
2023-10-17 13:13:06 -0300

    Buscador finalizado

--- FrontEnd/src/app/pages/home/components/cambios/cambios.component.ts
+++ FrontEnd/src/app/pages/home/components/cambios/cambios.component.ts
@@ -13,9 +13,11 @@ export class CambiosComponent implements OnInit {
 
   public loader:boolean = false;
 
-  public listNovedades:Novedad[] = [];
+  public listNovedades: { [mes: string]: Novedad[] } = {};
 
-  public filteredNovedades:Novedad[] = [];
+  public filteredNovedades: { [mes: string]: Novedad[] } = {};
+
+  public sorted:string[] = []
 
   public valoresColores: { [valor: string]: string } = {
     "Abacom": '#f73b3b',
@@ -23,10 +25,10 @@ export class CambiosComponent implements OnInit {
     "Jboss": '#605fb4',
     "Autorizador": '#e4be40',
     "Pepito": '#e4be40',
-    "SQL": '#e4be40',
-    "MongoDB": '#e4be40',
-    "Openshift": '#e4be40',
-    "Servidor": '#e4be40',
+    "SQL": '#g4ae50',
+    "MongoDB": '#f4be90',
+    "Openshift": '#f4be50',
+    "Servidor": '#a4be40',
   };
   
   constructor( 
@@ -48,13 +50,36 @@ export class CambiosComponent implements OnInit {
 
   getNovedades(){
     this.dbService.getNovedades().subscribe( novedades => {
-      this.listNovedades = novedades.sort((a,b) => {
+  
+      novedades.sort((a,b) =>{
         return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
       })
+  
+      novedades.forEach((novedad:Novedad) => {
+        const fecha = new Date(novedad.created_at);
+        const mes = fecha.toLocaleString('es-ES',{month:'long'});
+        const anio = fecha.getFullYear();
+        const key = `${mes}-${anio}`;
+        
+        if(!this.listNovedades[key]){
+          this.listNovedades[key] = [];
+        }
+        this.listNovedades[key].push(novedad);
+      })
 
-      this.filteredNovedades = [...this.listNovedades];
+      this.filteredNovedades = this.listNovedades;
+  
+      this.sorted = Object.keys(this.listNovedades).sort((a, b) => {
+        const fechaA = new Date(a.split('-')[1]);
+        const fechaB = new Date(b.split('-')[1]);
+        return fechaB.getTime() - fechaA.getTime();
+      });
     })
   }
+  
+  isEmpty(obj: object): boolean {
+    return Object.keys(obj).length === 0;
+  }  
 
   openDialog(item:Novedad){
     const dialog = this.dialog.open(DialogNovedadComponent,{
@@ -69,16 +94,28 @@ export class CambiosComponent implements OnInit {
 
   searchNovedades() {
     const newTag = this.searchInput.nativeElement.value.toLowerCase();
-
+  
     if (newTag) {
-      this.filteredNovedades = this.listNovedades.filter(novedad => {
-        return novedad.etiquetas.some(tag => tag.toLowerCase().includes(newTag));
+      this.filteredNovedades = {};
+
+      for (let mes in this.listNovedades) {
+        let filteredNovedades = this.listNovedades[mes].filter(novedad => {
+          return novedad.etiquetas.some(tag => tag.toLowerCase().includes(newTag));
+        });
+        if (filteredNovedades.length > 0) {
+          this.filteredNovedades[mes] = filteredNovedades;
+        }
+      }
+
+      this.sorted = Object.keys(this.filteredNovedades).sort((a, b) => {
+        const fechaA = new Date(a.split('-')[1]);
+        const fechaB = new Date(b.split('-')[1]);
+        return fechaB.getTime() - fechaA.getTime();
       });
-
-    } else {
-      this.filteredNovedades = [...this.listNovedades];
     }
-
   }
+  
+  
+  
   
 }
