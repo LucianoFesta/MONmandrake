889877b31309b582dff6516131b1bba25ce12def
Luciano Festa <lucianoefesta@hotmail.com>
2023-11-02 15:00:43 -0300

    Agregado de busqueda por titulo y desc en el metodo tagsandkeyword

--- BackEnd_Python/routers/novedades.py
+++ BackEnd_Python/routers/novedades.py
@@ -61,7 +61,15 @@ async def getListFilteredByKeywordAndTags(keyword:str, tags:List[str] = Query(..
     if not keyword and tags:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='No existen tags y palabra clave a buscar.')
     
-    listFiltered = db_client.novedads.find({"descripcion":{'$regex':keyword, '$options':'i'},'etiquetas':{'$all':tags} })
+    query = {
+        "$or": [
+            {"descripcion": {'$regex': keyword, '$options': 'i'}},
+            {"titulo": {'$regex': keyword, '$options': 'i'}}
+        ],
+        'etiquetas':{'$all':tags}
+    }
+    
+    listFiltered = db_client.novedads.find(query)
     listFiltered = sorted(listFiltered, key=lambda novedad:novedad['created_at'], reverse=True)
     
     result = [Novedad(**novedadSchema(item)) for item in listFiltered]
